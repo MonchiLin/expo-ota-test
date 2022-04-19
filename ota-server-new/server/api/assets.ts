d359c3f1374d3da4922291539968dc044d8aa6d6
kiki <lin912305043@gmail.com>
2022-04-19 19:52:00 +0800

    test

---
+++ ota-server-new/server/api/assets.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+import mime from 'mime';
+import path from 'path';
+
+import { getMetadataSync } from '../../../common/helpers';
+
+export default function assetsEndpoint(req, res) {
+  debugger
+  const { asset: assetName, runtimeVersion, platform } = req.query;
+
+  if (!assetName) {
+    res.statusCode = 400;
+    res.json({ error: 'No asset name provided.' });
+    return;
+  }
+
+  if (!platform) {
+    res.statusCode = 400;
+    res.json({ error: 'No platform provided. Expected "ios" or "android".' });
+    return;
+  }
+
+  if (!runtimeVersion) {
+    res.statusCode = 400;
+    res.json({ error: 'No runtimeVersion provided.' });
+    return;
+  }
+
+  const updateBundlePath = `updates/${runtimeVersion}`;
+  const { metadataJson } = getMetadataSync({
+    updateBundlePath,
+    runtimeVersion,
+  });
+
+  const assetPath = path.resolve(assetName);
+  const assetMetadata = metadataJson.fileMetadata[platform].assets.find(
+    (asset) => asset.path === assetName.replace(`updates/${runtimeVersion}/`, '')
+  );
+  const isLaunchAsset =
+    metadataJson.fileMetadata[platform].bundle ===
+    assetName.replace(`updates/${runtimeVersion}/`, '');
+
+  if (!fs.existsSync(assetPath)) {
+    res.statusCode = 404;
+    res.json({ error: `Asset "${assetName}" does not exist.` });
+    return;
+  }
+
+  try {
+    const asset = fs.readFileSync(assetPath, null);
+
+    res.statusCode = 200;
+    res.setHeader(
+      'content-type',
+      isLaunchAsset ? 'application/javascript' : mime.getType(assetMetadata.ext)
+    );
+    res.end(asset);
+  } catch (error) {
+    res.statusCode = 500;
+    res.json({ error });
+  }
+}
